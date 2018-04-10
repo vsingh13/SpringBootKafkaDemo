252388fa857143ab8071d2bb8c8971a4a3c17593
vsingh13 <vsingh13@sapient.com>
2018-04-10 13:31:06 +0100

    Initial Commit

--- src/test/java/com/sample/kafka/BootKafkaApplicationTests.java
+++ src/test/java/com/sample/kafka/BootKafkaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootApacheKafkaApplicationTests {
+public class BootKafkaApplicationTests {
 
 	@Test
 	public void contextLoads() {
